e531b10d4e387e224c3f2cbab198581afe478d2c
panfengyu <panfengyu@021.com>
2019-03-23 14:17:27 +0800

    第一次提交

---
+++ app/src/main/java/fy/learn/com/pagerindictor/MainActivity.java
@@ -0,0 +1,13 @@
+package fy.learn.com.pagerindictor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
